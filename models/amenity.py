44f45b1082d10b606e1c5207debca81806bb4ca4
Fatimah1403 <fattyhassan14@gmail.com>
2023-10-31 17:12:59 +0000

    Task adeed

--- models/amenity.py
+++ models/amenity.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
+    """Representation of Amenity class """
     if models.storage_t == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
